7f14c2bd3b0b693fffbc2727d57d4ec7e5b64183
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-08-31 20:01:18 +0600

    navbar & services section design half done

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,10 @@
+import daisyui from "daisyui"
+
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: ["@tailwindcss/postcss", daisyui],
+  daisyui: {
+    themes: ["light"]
+  }
 };
 
 export default config;
